c709a45a1793daf3c308309fb7e23c8ba0460d8f
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-10-25 01:14:31 +0200

    gofiles: file processor. Process content as CSV

--- processor/processor.go
+++ processor/processor.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
 )
+
+type fileInfo struct {
+	id  string
+	seq int
+}
+
+type fileInfoResult struct {
+	id   string
+	seqs []int
+}
 
 func main() {
 	fmt.Println("File processor")
@@ -30,23 +42,64 @@ func main() {
 	PrintFatalError(err, "Error creating the unprocessed file")
 	defer unprocessedFile.Close()
 
-	processFile(originFile, destinyFile, errorFile)
+	fileInfos := processFile(originFile, errorFile)
+
+	processResult(destinyFile, errorFile, fileInfos)
 
 	fmt.Println("END File processor")
 
 }
 
-func processFile(originFile, destinyFile, errorFile *os.File) {
-	scanner := bufio.NewScanner(originFile)
+func processFile(originFile, errorFile *os.File) []fileInfo {
+	fileInfos := []fileInfo{}
+	r := csv.NewReader(originFile)
+	r.Comma = ';'
+	r.Comment = '#'
 	count := 0
-	for scanner.Scan() {
+	//var previous fileInfo
+	for {
 		count++
-		_, err := destinyFile.WriteString(scanner.Text() + "\n")
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
 		if err != nil {
-			_, err := errorFile.WriteString(err.Error())
-			PrintFatalError(err, "Error writing the error of processing the line "+string(count))
+			_, err := errorFile.WriteString(fmt.Sprintf("Error at line %d: %v", count, err))
+			PrintFatalError(err, fmt.Sprintf("Error at processing file. Error writing the error of processing the line %d", count))
+		}
+		seq, err := strconv.Atoi(record[1])
+		if err != nil {
+			_, err := errorFile.WriteString(fmt.Sprintf("Error at line %d: %v", count, err))
+			PrintFatalError(err, fmt.Sprintf("Error at processing file. Error writing the error of processing the line %d", count))
+		}
+		fileInfo := fileInfo{record[0], seq}
+		fileInfos = append(fileInfos, fileInfo)
+	}
+	fmt.Printf("Processed from file: %d\n", len(fileInfos))
+	return fileInfos
+}
+
+func processResult(destinyFile, errorFile *os.File, fileInfos []fileInfo) {
+	w := csv.NewWriter(destinyFile)
+
+	count := 0
+	for _, fileInfo := range fileInfos {
+		count++
+		record := []string{fileInfo.id, strconv.Itoa(fileInfo.seq)}
+		if err := w.Write(record); err != nil {
+			_, err := errorFile.WriteString(fmt.Sprintf("Error at line %d: %v", count, err))
+			PrintFatalError(err, fmt.Sprintf("Error at processing resultfile. Error writing the error of processing the line %d", count))
 		}
 	}
+
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		_, err := errorFile.WriteString(fmt.Sprintf("Error with CSV %v", err))
+		PrintFatalError(err, fmt.Sprintf("Error at processing resultfile. Error writing the error of processing the line %d", count))
+	}
+
+	fmt.Printf("Processed to file: %d\n", count)
 }
 
 //PrintFatalError Utility to print an error with a message with a log fatal (it exits the program)
